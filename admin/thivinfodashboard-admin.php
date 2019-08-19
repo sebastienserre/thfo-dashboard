d67f97a46504235f703209b1f38469ab9c99f2dd
sebastienserre <sebastien@thivinfo.com>
2019-08-19 16:57:40 +0200

    load function correctly

--- admin/thivinfodashboard-admin.php
+++ admin/thivinfodashboard-admin.php
@@ -1,18 +1,7 @@
 <?php
-/**
- * @since      1.0
- *
- * @package    thivinfodashboard
- * @subpackage thivinfodashboard/admin
- */
-
-/**
- * The admin-specific functionality of the plugin.
- *
- * @package    thivinfodashboard
- * @subpackage thivinfodashboard/admin
- * @author     sebastienserre <sebastien@thivinfo.com>
- */
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+} // Exit if accessed directly.
 
 /**
  *
@@ -267,35 +256,4 @@ function thivinfo_main_dashboard_widget() {
     </div>
     </div>
 	<?php
-}
-
-/**
- * Get WP Direct filesystem object. Also define chmod constants if not done yet.
- *
- * @since 1.0
- * @since 1.3 Don't use the global Filesystem anymore, to make sure to use "direct" (some things don't work over "ftp").
- *
- * @return `$wp_filesystem` object.
- */
-function thfo_get_filesystem() {
-	static $filesystem;
-
-	if ( $filesystem ) {
-		return $filesystem;
-	}
-
-	require_once( ABSPATH . 'wp-admin/includes/class-wp-filesystem-base.php' );
-	require_once( ABSPATH . 'wp-admin/includes/class-wp-filesystem-direct.php' );
-
-	$filesystem = new WP_Filesystem_Direct( new StdClass() ); // WPCS: override ok.
-
-	// Set the permission constants if not already set.
-	if ( ! defined( 'FS_CHMOD_DIR' ) ) {
-		define( 'FS_CHMOD_DIR', ( @fileperms( ABSPATH ) & 0777 | 0755 ) );
-	}
-	if ( ! defined( 'FS_CHMOD_FILE' ) ) {
-		define( 'FS_CHMOD_FILE', ( @fileperms( ABSPATH . 'index.php' ) & 0777 | 0644 ) );
-	}
-
-	return $filesystem;
 }
