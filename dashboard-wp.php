cee6141aa1edc0d1963e8711253a029ac7a2d3f9
sebastienserre <sebastien@thivinfo.com>
2019-10-03 21:29:59 +0200

    correct $msg render

--- dashboard-wp.php
+++ dashboard-wp.php
@@ -3,7 +3,7 @@
  * Plugin Name:       WordPress Dashboard
  * Plugin URI:        https://thivinfo.com
  * Description:       Thivinfo Custom Dashboard for WordPress.
- * Version:           1.2.4.3
+ * Version:           1.2.4.4
  * Author:            Thivinfo
  * Author URI:        https://thivinfo.com
  * License:           GPL-2.0+
